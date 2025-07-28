0824c16865cb9bda716ca388f9472624849fc3b8
eugene-pande <esp.pandev@gmail.com>
2025-07-28 12:39:04 +1000

    Updated HomeController to retrive and display projects to homepage

--- app/Http/Controllers/Public/HomeController.php
+++ app/Http/Controllers/Public/HomeController.php
@@ -5,16 +5,22 @@ namespace App\Http\Controllers\Public;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\NewsUpdate;
+use App\Models\Project;
 
 class HomeController extends Controller
 {
-    public function showNewsUpdates()
+    public function index()
     {
         $newsUpdates = NewsUpdate::published()
                         ->latest('published_date')
                         ->take(6)
                         ->get();
 
-        return view('public.home', compact('newsUpdates'));
+        $projects = Project::where('approved', true)
+                    ->orderBy('created_at', 'desc')
+                    ->take(3)
+                    ->get();
+
+        return view('public.home', compact('newsUpdates', 'projects'));
     }
 }
