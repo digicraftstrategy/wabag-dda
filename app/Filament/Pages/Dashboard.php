4f2c5da51b3fd3add9884e253403c2fe15bea2b8
nshoresh <nshoresh@yumicode.tech>
2025-07-31 15:01:24 +0300

    Pie Chart Added

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -4,28 +4,38 @@ namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
 use Illuminate\Support\Facades\Auth;
-use App\Filament\Widgets\{ProjectKpiOverview, ProjectTimelineChart, RecentNewsUpdates, ProjectStatusPieChart};
+use App\Filament\Widgets\{
+    ProjectKpiOverview,
+    ProjectTimelineChart,
+    RecentNewsUpdates,
+    ProjectStatusPieChart
+};
 
 class Dashboard extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-home';
-
     protected static string $view = 'filament.pages.project-tracker-dashboard';
+    //protected static ?string $title = 'Project Tracker Dashboard';
+
+    /*public static function getSlug(): string
+    {
+        return 'custom-dashboard'; // Unique route slug
+    }*/
 
     public static function shouldRegisterNavigation(): bool
     {
         /** @var User|null $user */
         $user = Auth::user();
-        //return auth()->user()?->hasRole('admin') || auth()->user()?->can('view dashboard'); 
-        return $user && $user->hasRole('admin') || $user && $user->can('view dashboard');
+        return $user && ($user->hasRole('admin') || $user->can('view dashboard'));
     }
+
     protected function getHeaderWidgets(): array
     {
         return [
-                ProjectKpiOverview::class,
-                ProjectTimelineChart::class,
-                ProjectStatusPieChart::class,
-                RecentNewsUpdates::class,
+            ProjectKpiOverview::class,
+            ProjectTimelineChart::class,
+            ProjectStatusPieChart::class,
+            RecentNewsUpdates::class,
         ];
     }
 }
